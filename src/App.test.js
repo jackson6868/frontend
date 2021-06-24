6b1ac4e8a337dcb60eecd3d663db3bec47bc501b
jackson6868 <tutus@DESKTOP-TLC7AE3.localdomain>
2021-06-24 22:35:24 +0200

    add
     Committer: jackson6868 <tutus@DESKTOP-TLC7AE3.localdomain>
     Changes to be committed:
    	new file:   .travis.yml
    	modified:   src/App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/dchomik/);
+  const linkElement = screen.getByText(/chomik/);
   expect(linkElement).toBeInTheDocument();
 });
 
